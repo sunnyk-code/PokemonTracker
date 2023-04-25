2dd477cead899fb76e103366c42d0e03b8a40a04
Rohit Prasanna <rohit.prasanna1@gmail.com>
2023-04-24 20:10:56 -0400

    completed log screen completely

--- navigation/screens/LogScreen.js
+++ navigation/screens/LogScreen.js
@@ -3,12 +3,15 @@ import SearchableDropdown from 'react-native-searchable-dropdown';
 import { 
   View, 
   Text, 
-  FlatList, 
   Image,
-  TextInput, 
+  TextInput,
   StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Button,
+  TouchableOpacity
 } from 'react-native';
-import DropDownPicker from 'react-native-dropdown-picker';
+import Checkbox from 'expo-checkbox'
 
 class LogScreen extends Component {
   constructor(props) {
@@ -16,9 +19,15 @@ class LogScreen extends Component {
     this.state = {
       pokemonList: [],
       selectedPokemon: null,
-      cp: '',
-      location: '',
-      searchText: ''
+      cp: null,
+      attackIV: null,
+      defenseIV: null,
+      hpIV: null,
+      searchText: '',
+      overThirty: false,
+      isFocused: false,
+      isBlurred: true,
+      isPressed: false
     };
   }
 
@@ -27,12 +36,6 @@ class LogScreen extends Component {
     const json = await response.json();
 
     this.setState({ pokemonList: Object.values(json) });
-
-    // fetch('https://pogoapi.net/api/v1/released_pokemon.json')
-    //   .then(response => response.json())
-    //   .then(pokeList => {
-    //     this.setState({ pokemonList: Object.values(pokeList) });
-    //   });
   }
 
   render() {
@@ -45,103 +48,205 @@ class LogScreen extends Component {
       });
     }
 
-    console.log(filteredPokemon);
 
     return (
-      <View style={styles.container}>
-        <View style={styles.sidePanel}>
-          <Text style={styles.title}>Pokemon Details:</Text>
-          <Text style={styles.text}>Select a Pokemon: </Text>
-          <View style={styles.searchContainer}>
-            <SearchableDropdown
-              onItemSelect={(item) => {
-                this.setState({ selectedPokemon: item });
-              }}
-              onTextChange={text => {
-                this.setState({ searchText: text });
-              }}
-              itemStyle={{
-                padding: 7.5,
-                marginTop: 0,
-                backgroundColor: '#ddd',
-                borderColor: '#bbb',
-                borderWidth: 1,
-                borderRadius: 5,
-              }}
-              itemTextStyle={{ color: '#222' }}
-              itemsContainerStyle={{ maxHeight: 140 }}
-              items={filteredPokemon}
-              resetValue={false}
-              textInputProps={
-              {
-                placeholder: "Search",
-                underlineColorAndroid: "transparent",
-                style: {
-                    padding: 12,
-                    borderWidth: 1,
-                    borderColor: '#ccc',
-                    borderRadius: 5,
-                }
-              }
-            }
-              listProps={
-                {
-                  nestedScrollEnabled: false,
-                }
-              }
-            />
-            </View>
-        </View>
-        {/* <View style={styles.inputContainer}>
-          <TextInput
-              style={styles.input}
-              placeholder="Combat Power"
-              // onChangeText={(text) => this.setState({cp: text})}
-              value={this.state.cp}
-          />
-          <TextInput
-              style={styles.input}
-              placeholder="Location"
-              // onChangeText={(text) => this.setState({location: text})}
-              value={this.state.location}
-          />
-        </View> */}
-        {/* <FlatList
-          data={filteredPokemon}
-          renderItem={({ item }) => (
-            <View style={styles.listItem}>
-              <Image
-                style={styles.image}
-                source={{
-                  uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.url.split('/')[6]}.png`,
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} style={{zIndex: -10}}>
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.title}>Pokemon Details:</Text>
+            <Text style={styles.text}>Select a Pokemon: </Text>
+            <View style={styles.searchContainer}>
+              <SearchableDropdown
+                onItemSelect={(item) => {
+                  this.setState({ selectedPokemon: item });
                 }}
+                onTextChange={text => {
+                  this.setState({ searchText: text });
+                }}
+                itemStyle={{
+                  padding: 7.5,
+                  marginTop: 0,
+                  backgroundColor: '#ddd',
+                  borderColor: '#bbb',
+                  borderWidth: 1,
+                  borderRadius: 5
+                }}
+                itemTextStyle={{ color: '#222' }}
+                itemsContainerStyle={{ maxHeight: 140}}
+                items={filteredPokemon}
+                resetValue={false}
+                onFocus={() => {
+                  this.setState({ isFocused: true, isBlurred: false });
+                }}
+                onBlur={() => {
+                  this.setState({ isFocused: false, isBlurred: true });
+                }}
+                textInputProps={
+                  {
+                    value: this.state.selectedPokemon ? this.state.selectedPokemon.name : null,
+                    placeholder: "Search",
+                    underlineColorAndroid: "transparent",
+                    style: {
+                      padding: 12,
+                      borderWidth: 1,
+                      borderColor: '#333',
+                      borderRadius: 5,
+                    },
+                  }
+              }
+                modal={true}
+                listProps={
+                  {
+                    nestedScrollEnabled: true,
+                  }
+                }
               />
-              <Text
-                style={styles.text}
-                onPress={() => this.setState({ selectedPokemon: item })}
-              >
-                {item.name}
-              </Text>
             </View>
-          )}
-          keyExtractor={item => item.name}
-        /> */}
-        
-        {/* <View>
-        {this.state.selectedPokemon && (
-          <View style={styles.selectedPokemon}>
-            <Image
-              style={styles.selectedImage}
-              source={{
-                uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.state.selectedPokemon.url.split('/')[6]}.png`,
-              }}
+            <View style={styles.checkbox}>
+              <Checkbox
+                disabled={false}
+                value={this.state.overThirty}
+                onValueChange={(newValue) => this.setState({ overThirty: newValue })}
+                marginTop={10}
+                zIndex={1}
             />
-            <Text style={styles.selectedText}>{this.state.selectedPokemon.name}</Text>
+            <Text style={{marginLeft: 8, marginTop: 9, zIndex: 1}}>Trainer Level 30+?</Text>
+            </View>
           </View>
-        )}
-        </View> */}
-      </View>
+          
+          <View style={[{marginTop: -55}, this.state.isFocused && styles.zIndexFocused, this.state.isBlurred && styles.zIndexBlurred]}>
+            {this.state.overThirty && (
+              <View>
+                <Text style={styles.text}>Combat Power: </Text>
+                <View style={{ flex: 'column'}}>
+                  <TextInput
+                      disabled={false}
+                      editable={true}
+                      style={styles.input}
+                      placeholder="Combat Power"
+                      keyboardType="numeric"
+                      onChangeText={(text) => {
+                        let editedText = parseInt(text.replace(/[^0-9]/g, '')).toString();
+                        if (editedText === 'NaN') {
+                          editedText = '';
+                        }
+                        this.setState({ cp: editedText });  
+                        }}
+                      value={this.state.cp}
+                  />
+                  <Text style={[{ marginTop: 10 }, styles.text]}>IVs: </Text>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <TextInput
+                      disabled={false}
+                      editable={true}
+                      style={styles.ivInput}
+                      placeholder="Attack"
+                      keyboardType="numeric"
+                      onChangeText={(text) => {
+                        let editedText = parseInt(text.replace(/[^0-9]/g, '')).toString();
+                        if (editedText === 'NaN') {
+                          editedText = '';
+                        }
+                        if (parseInt(editedText) > 15) {
+                          editedText = '15';
+                        }
+                        this.setState({ attackIV: editedText });  
+                        }}
+                      value={this.state.attackIV}
+                  />
+                  <TextInput
+                      disabled={false}
+                      editable={true}
+                      style={styles.ivInput}
+                      placeholder="Defense"
+                      keyboardType="numeric"
+                      onChangeText={(text) => {
+                        let editedText = parseInt(text.replace(/[^0-9]/g, '')).toString();
+                        if (editedText === 'NaN') {
+                          editedText = '';
+                        }
+                        if (parseInt(editedText) > 15) {
+                          editedText = '15';
+                        }
+                        this.setState({ defenseIV: editedText });  
+                        }}
+                      value={this.state.defenseIV}
+                  />
+                  <TextInput
+                      disabled={false}
+                      editable={true}
+                      style={styles.ivInput}
+                      placeholder="HP"
+                      keyboardType="numeric"
+                      onChangeText={(text) => {
+                        let editedText = parseInt(text.replace(/[^0-9]/g, '')).toString();
+                        if (editedText === 'NaN') {
+                          editedText = '';
+                        }                        
+                        if (parseInt(editedText) > 15) {
+                          editedText = '15';
+                        }
+                        this.setState({ hpIV: editedText });  
+                        }}
+                      value={this.state.hpIV}
+                    />
+                  </View>
+                </View>
+              </View>
+            )}
+          </View>
 
+          <View style={{ zIndex: -2 }}>
+            {this.state.selectedPokemon && (
+              <View style={styles.selectedPokemonContainer}>
+                <View style={{ flexDirection: 'row', marginTop: 10, alignContent: 'center' }}>
+                  <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+                    <Text style={styles.selectedText}>{this.state.selectedPokemon.name}</Text>
+                    <Image
+                      style={styles.selectedImage}
+                      source={{
+                        uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.state.selectedPokemon.id}.png`,
+                      }}
+                    />
+                  </View>
+                  <View style={{ flexDirection: 'column', alignItems: 'center', marginTop: 30}}>
+                    {this.state.cp && this.state.overThirty && this.state.defenseIV
+                      && this.state.attackIV && this.state.hpIV && (
+                      <Text style={styles.cpIvText}>CP: {this.state.cp}</Text>
+                    )}
+                    {this.state.cp && this.state.overThirty && this.state.defenseIV
+                      && this.state.attackIV && this.state.hpIV && (
+                      <Text style={styles.cpIvText}>Attack IV: {this.state.attackIV}</Text>
+                    )}
+                    {this.state.cp && this.state.overThirty && this.state.defenseIV
+                      && this.state.attackIV && this.state.hpIV && (
+                      <Text style={styles.cpIvText}>Defense IV: {this.state.defenseIV}</Text>
+                    )}
+                    {this.state.cp && this.state.overThirty && this.state.defenseIV
+                      && this.state.attackIV && this.state.hpIV && (
+                      <Text style={styles.cpIvText}>HP IV: {this.state.hpIV}</Text>
+                    )}    
+                  </View>
+                </View>
+              </View>
+            )}
+            {((this.state.selectedPokemon && !this.state.overThirty) || (this.state.selectedPokemon
+              && this.state.overThirty && this.state.cp && this.state.attackIV && this.state.defenseIV && this.state.hpIV))
+              && (
+              <TouchableOpacity 
+              onPressIn={() => {
+                this.state.isPressed = true;
+              }}
+              onPressOut={() => {
+                this.state.isPressed = false;
+              }}
+              style={[styles.button, this.state.isPressed && styles.pressedButton]}
+            >
+              <Text style={styles.buttonText}>Log</Text>
+            </TouchableOpacity>)}
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -152,6 +257,23 @@ const styles = StyleSheet.create({
     paddingTop: 30,
     paddingHorizontal: 20,
   },
+  zIndexFocused: {
+    zIndex: -10
+  },
+  zIndexBlurred: {
+    zIndex: 10
+  },
+  cpIvText: {
+    fontSize: 17.5,
+    fontWeight: 'bold',
+    color: '#333',
+    alignSelf: 'center',
+    marginTop: 10
+  },
+  checkbox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   title: {
     fontSize: 27.5,
     fontWeight: 'bold',
@@ -161,9 +283,20 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 17.5,
+    zIndex: 1,
   },
   input: {
-    width: '100%',
+    width: '75%',
+    height: 40,
+    backgroundColor: '#FFF',
+    borderColor: '#333',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginTop: 10
+  },
+  ivInput: {
+    width: '25%',
     height: 40,
     backgroundColor: '#FFF',
     borderColor: '#333',
@@ -181,71 +314,46 @@ const styles = StyleSheet.create({
     marginTop: 12.5,
     backgroundColor: '#FFF',
     borderRadius: 5,
-    height: '20%',
-    width: '75%'
-  },
-  searchInput: {
-    flex: 1,
-    height: '100%',
-    paddingHorizontal: 10,
-    color: '#333'
+    height: '19%',
+    width: '75%',
+    zIndex: 100
   },
   searchButton: {
     backgroundColor: '#333',
     padding: 10,
     borderRadius: 5
   },
-  searchButtonText: {
-    color: '#FFF',
-    fontWeight: 'bold',
-    fontSize: 16
-  },
-  pokemonContainer: {
-    flex: 1,
-    width: '100%',
-    paddingHorizontal: 10,
-    paddingBottom: 20,
-    marginTop: 20
-  },
-  pokemonItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    elevation: 3
-  },
-  
-  pokemonName: {
-    fontSize: 16,
-    color: '#333'
-  },
   selectedPokemonContainer: {
-    flex: 1.5,
-    marginTop: 20,
-    marginLeft: 100,
+    marginTop: 15,
     alignItems: 'center',
+    zIndex: 1
   },
   selectedImage: {
-    width: 250,
-    height: 250,
+    width: 225,
+    height: 225,
     borderRadius: 125,
-    marginBottom: 10
-  },
-  selectedPokemonName: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 10,
-    color: '#333'
+    marginTop: -17.5,
+    alignSelf: 'center',
   },
   selectedText: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 10,
-    color: '#333'
-  }
+    color: '#333',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  pressedButton: {
+    backgroundColor: '#3E8E41',
+  },
 });
 
 export default LogScreen;
